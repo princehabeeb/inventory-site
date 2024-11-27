2784fc006302842e0dd39f52457319fa70a27df6
rozzdev <lightsamuel10@gmail.com>
2024-11-27 03:00:40 -0800

    update

--- script.js
+++ script.js
@@ -7,26 +7,29 @@ let amount = document.getElementById("amount");
 let date = document.getElementById("date");
 
 submit.addEventListener("click", () => {
-    if(itemName.value.trim)
+    if(itemName.value.trim() !== ""){
+        active()
+        itemName.value = "";
+    }
 });
 
 const active = (e) => {
     let div = document.createElement("div");
-    div.classList.add("container")
+    div.classList.add("container");
     let img = document.createElement("img");
     img.src = image;
     let paraOne = document.createElement("p");
-    paraOne.setAttribute("id", "name=active");
-    paraOne.innerText = e;
+    paraOne.setAttribute("id", "name-active");
+    paraOne.innerText = itemName.value;
     let paraTwo = document.createElement("p");
     paraTwo.setAttribute("id", "number-active");
-    paraTwo.innerText = number;
+    paraTwo.innerText = number.value;
     let paraThree = document.createElement("p");
     paraThree.setAttribute("id", "amount-active");
-    paraThree.innerText = amount;
+    paraThree.innerText = amount.value;
     let paraFour = document.createElement("p");
     paraFour.setAttribute("id", "date-active");
-    paraFour.innerText = date;
+    paraFour.innerText = date.value;
 
     div.appendChild(img);
     div.appendChild(paraOne);
